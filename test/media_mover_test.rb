2d5e9dd7d61ad7673637231d0f5be7402c2e064c
Luke Redpath <contact@lukeredpath.co.uk>
2009-05-18 02:14:08 +0100

    Support format [SHOW NAME] - [META].
    Added an option to skip the file-size check as this forces a single run
    to be a minimum of 5 seconds. We don't need or want this when testing as
    it makes the tests too slow.

--- test/media_mover_test.rb
+++ test/media_mover_test.rb
@@ -23,6 +23,7 @@ class MediaMoverTest < Test::Unit::TestCase
   
   def teardown
     rm_rf File.join(File.dirname(__FILE__), *%w[tmp])
+    rm_f "/tmp/media-mover.lock"
   end
   
   context "Moving TV shows" do
@@ -41,10 +42,38 @@ class MediaMoverTest < Test::Unit::TestCase
       assert File.exist?(File.join(@tv_dir, 'The.Simpsons', 'Season 2', 'The.Simpsons.S02E13.avi'))
     end
     
-  end  
+    should "move files matching [TV SHOW] - SXEYY.avi" do
+      show_name = "The Simpsons - S02E13.avi"
+      touch File.join(@downloads_dir, show_name)
+      assert do_media_mover!
+      assert File.exist?(File.join(@tv_dir, 'The.Simpsons', 'Season 2', 'The.Simpsons.S02E13.avi'))
+    end
+    
+    should "move files matching [TV SHOW].SxEE.avi" do
+      show_name = "The Simpsons.2x13.avi"
+      touch File.join(@downloads_dir, show_name)
+      assert do_media_mover!
+      assert File.exist?(File.join(@tv_dir, 'The.Simpsons', 'Season 2', 'The.Simpsons.S02E13.avi'))
+    end
+    
+    should "move files matching [TV SHOW] SxEE.avi" do
+      show_name = "The Simpsons 2x13.avi"
+      touch File.join(@downloads_dir, show_name)
+      assert do_media_mover!
+      assert File.exist?(File.join(@tv_dir, 'The.Simpsons', 'Season 2', 'The.Simpsons.S02E13.avi'))
+    end
+    
+    should "move files matching [TV SHOW] - SxEE.avi" do
+      show_name = "The Simpsons - 2x13.avi"
+      touch File.join(@downloads_dir, show_name)
+      assert do_media_mover!
+      assert File.exist?(File.join(@tv_dir, 'The.Simpsons', 'Season 2', 'The.Simpsons.S02E13.avi'))
+    end
+    
+  end
   
   def do_media_mover!(other_options = '')
     bin_path = File.join(File.dirname(__FILE__), *%w[.. bin media-mover])
-    system("perl #{bin_path} -no-update-plex -d #{@downloads_dir} -t #{@tv_dir} -m #{@movie_dir} #{other_options}")
+    system("perl #{bin_path} -no-update-plex -k -d #{@downloads_dir} -t #{@tv_dir} -m #{@movie_dir} #{other_options}")
   end
 end
